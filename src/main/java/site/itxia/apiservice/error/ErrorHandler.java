bfb106209b1a00bf0f239c1ce495243c549f86e8
Zhenxi <PiscesXP@outlook.com>
2019-10-26 14:57:43 +0800

    添加json解析错误异常处理

--- src/main/java/site/itxia/apiservice/error/ErrorHandler.java
+++ src/main/java/site/itxia/apiservice/error/ErrorHandler.java
@@ -1,6 +1,7 @@
 package site.itxia.apiservice.error;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,14 +16,24 @@ import site.itxia.apiservice.dto.ResultWrapper;
 @ControllerAdvice
 public class ErrorHandler {
 
-    /*
+    /**
      * 处理参数校验产生的错误.
-     * */
+     */
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ResultWrapper<String> errorHandler(MethodArgumentNotValidException e) {
+    public ResultWrapper<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         var result = new ResultWrapper<String>(20, e.getBindingResult().getFieldError().getDefaultMessage());
         return result;
+    }
+
+    /**
+     * 处理JSON解析错误.
+     */
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResultWrapper<String> httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
+        return new ResultWrapper<>(20, e.getMessage());
     }
 }
