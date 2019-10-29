c9de40ed92b28740cbc9f3c0f9d7153659a9375c
Zhenxi <PiscesXP@outlook.com>
2019-10-29 23:42:12 +0800

    member表增加字段定义

--- src/main/java/site/itxia/apiservice/data/entity/Member.java
+++ src/main/java/site/itxia/apiservice/data/entity/Member.java
@@ -14,16 +14,19 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "real_name")
+    @Column(name = "real_name", nullable = false)
     private String realName;
 
-    @Column(name = "login_name")
+    @Column(name = "login_name", nullable = false)
     private String loginName;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false, columnDefinition = "bit(2) default 0")
     private int role;
 
+    @Column(nullable = false, columnDefinition = "bit(1) default 0")
     private int status;
 
 }
