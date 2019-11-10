04c82cf42d3e87ed9a56f676db090f123c816d12
Zhenxi <PiscesXP@outlook.com>
2019-11-10 08:58:08 +0800

    添加tag实体

---
+++ src/main/java/site/itxia/apiservice/data/entity/OrderTag.java
@@ -0,0 +1,33 @@
+package site.itxia.apiservice.data.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author zhenxi
+ */
+@Data
+@Entity
+@Table(name = "order_tag")
+public class OrderTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "order_id", nullable = false)
+    private int orderID;
+
+    @Column(name = "tag_id", nullable = false)
+    private int tagID;
+
+    @Column(name = "add_by", nullable = false)
+    private int addByMemberID;
+
+    @Column(name = "add_time", nullable = false)
+    private int addTime;
+
+    @Column(nullable = false)
+    private boolean delete;
+}
---
+++ src/main/java/site/itxia/apiservice/data/entity/Tag.java
@@ -0,0 +1,31 @@
+package site.itxia.apiservice.data.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author zhenxi
+ */
+@Data
+@Entity
+@Table(name = "tag")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "tag_name", nullable = false)
+    private String tagName;
+
+    @Column(name = "add_by", nullable = false)
+    private int addByMemberID;
+
+    @Column(name = "add_time", nullable = false)
+    private int addTime;
+
+    @Column(nullable = false)
+    private boolean delete;
+
+}
