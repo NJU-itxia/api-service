44219a1e1d57bcc3ebe3580629a7fccee0b24fb5
Zhenxi <PiscesXP@outlook.com>
2019-11-11 22:13:06 +0800

    修改member请求格式

--- src/main/java/site/itxia/apiservice/controller/MemberController.java
+++ src/main/java/site/itxia/apiservice/controller/MemberController.java
@@ -2,13 +2,11 @@ package site.itxia.apiservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import site.itxia.apiservice.dto.MemberDTO;
 import site.itxia.apiservice.dto.ResultWrapper;
 import site.itxia.apiservice.service.MemberService;
 import site.itxia.apiservice.vo.MemberAddVo;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/member")
@@ -18,14 +16,18 @@ public class MemberController {
     private MemberService memberService;
 
     @GetMapping("")
-    public List<MemberDTO> getAll(){
-        return memberService.getAllMember();
+    public ResultWrapper getSelf(@RequestHeader int memberID) {
+        return ResultWrapper.wrapSuccess(memberService.getMemberNameByID(memberID));
+    }
+
+    @GetMapping("/all")
+    public ResultWrapper getAll() {
+        return ResultWrapper.wrapSuccess(memberService.getAllMember());
     }
 
     @PostMapping("")
-    public ResultWrapper add(@Valid @RequestBody MemberAddVo memberAddVo){
+    public ResultWrapper add(@Valid @RequestBody MemberAddVo memberAddVo) {
         return memberService.addNewMember(memberAddVo);
     }
-
 
 }
