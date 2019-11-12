89baf8f04e7bc345587585b8ac25ef60d23ad71b
Zhenxi <PiscesXP@outlook.com>
2019-11-12 15:39:08 +0800

    添加MissingRequestHeaderException处理

--- src/main/java/site/itxia/apiservice/error/ErrorHandler.java
+++ src/main/java/site/itxia/apiservice/error/ErrorHandler.java
@@ -3,6 +3,7 @@ package site.itxia.apiservice.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,5 +36,15 @@ public class ErrorHandler {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ResultWrapper httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
         return ResultWrapper.wrap(ErrorCode.JSON_PARSE_ERROR, e.getMessage());
+    }
+
+    /**
+     * 处理缺少header错误.
+     * 因为使用memberID header来标识身份，所以一般是身份认证过期引起的.
+     */
+    @ExceptionHandler(value = MissingRequestHeaderException.class)
+    @ResponseBody
+    public ResultWrapper missingRequestHeaderExceptionHandler(MissingRequestHeaderException e) {
+        return ResultWrapper.wrap(ErrorCode.UNAUTHORIZED, "身份认证失败, 请尝试重新登录.");
     }
 }
